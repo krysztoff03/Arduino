5df6d004bd314a40a6a481c46d9aaa125040c7ad
krysztoff03 <cristi.iosif03@gmail.com>
2017-11-30 18:41:24 +0200

    Add files via upload

---
+++ CAN_Pot_Servo_Sender_v2/CAN_Pot_Servo_Sender_v2.ino
@@ -0,0 +1,47 @@
+// CAN Send Example
+//
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+MCP_CAN CAN0(53);     // Set CS to pin 10
+
+#define POTPIN A0
+int val;
+byte char_val;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
+  else Serial.println("Error Initializing MCP2515...");
+
+  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+}
+
+
+
+void loop()
+{
+  val = analogRead(POTPIN);
+  char_val = (byte)(val / 4);
+  Serial.print("Pot Value: ");
+  Serial.print(val);
+  Serial.print("Char Pot Value: ");
+  Serial.print(char_val);
+  byte data[8] = {char_val, 255, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(0x123, 0, 8, data);
+  if(sndStat == CAN_OK){
+    Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(15);   // send data per 100ms
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
